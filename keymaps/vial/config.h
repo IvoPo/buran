1ab30a694ab996d808940e8c467ab34487e4cea1
IvoPo <selfkiller@hotmail.com>
2022-09-19 23:11:27 +0200

    init

---
+++ keymaps/vial/config.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3F, 0x25, 0xD9, 0xFC, 0xA2, 0x98, 0x8F, 0x98}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 10 }
+
+// Vial "limits"
+#define VIAL_TAP_DANCE_ENTRIES 32
+#define VIAL_COMBO_ENTRIES 32
+#define VIAL_KEY_OVERRIDE_ENTRIES 32
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
